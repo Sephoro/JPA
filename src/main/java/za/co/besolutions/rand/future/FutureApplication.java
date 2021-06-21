fdd3c1e3a065e0dfaca3515e6501fbfbfc6c21a1
sephoro <eliassepuru@gmail.com>
2021-06-21 15:30:56 +0000

    Updates

--- src/main/java/za/co/besolutions/rand/future/FutureApplication.java
+++ src/main/java/za/co/besolutions/rand/future/FutureApplication.java
@@ -1,14 +1,22 @@
 package za.co.besolutions.rand.future;
 
+import jdk.swing.interop.SwingInterOpUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import za.co.besolutions.rand.future.entity.User;
 
-@SpringBootApplication
+@SpringBootApplication // spring context, autoconfiguration, component scan
 public class FutureApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FutureApplication.class, args);
+		ApplicationContext context = SpringApplication.run(FutureApplication.class, args);
+
+		int counter = 0;
+
+		for (String bean: context.getBeanDefinitionNames()) {
+			System.out.println(counter + " --->  " + bean);
+		}
 	}
 
 }
